6c79999bfdcd26b5564cd0997dbeb6bd31cda7bb
dzmitry yermakou <dyermakov@twelvedevs.com>
2023-03-27 09:37:26 +0200

    HW5

--- src/s2-homeworks/hw05/sidebar/Sidebar.tsx
+++ src/s2-homeworks/hw05/sidebar/Sidebar.tsx
@@ -1,58 +1,62 @@
-import React, {FC} from 'react'
-import {NavLink} from 'react-router-dom'
-import s from './Sidebar.module.css'
-import {PATH} from '../Pages'
-import closeIcon from './closeOutline.svg'
+import React, { FC } from "react";
+import { NavLink, useLocation } from "react-router-dom";
+import s from "./Sidebar.module.css";
+import { PATH } from "../Pages";
+import closeIcon from "./closeOutline.svg";
 
 type PropsType = {
-    open: boolean
-    handleClose: () => void
-}
+  open: boolean;
+  handleClose: () => void;
+};
 
-export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
-    const sidebarClass = s.sidebar
-        + (open ? ' ' + s.open : '')
-    return (
-        <>
-            {/*затемнение справа от открытого меню*/}
-            {open && <div className={s.background} onClick={handleClose}/>}
+export const Sidebar: FC<PropsType> = ({ open, handleClose }) => {
+  const sidebarClass = s.sidebar + (open ? " " + s.open : "");
+  let location = useLocation();
+  let juniorClass = location.pathname === PATH.JUNIOR;
+  let preJuniorClass = location.pathname === PATH.PRE_JUNIOR;
+  let juniorPlusClass = location.pathname === PATH.JUNIOR_PLUS;
 
-            <aside className={sidebarClass}>
-                <button className={s.close} onClick={handleClose}>
-                    <img
-                        src={closeIcon}
-                        alt="close sidebar"
-                        id={'hw5-menu-close'}
-                    />
-                </button>
+  console.log( location.pathname === PATH.JUNIOR);
 
-                <nav id={'hw5-menu'} className={s.nav}>
-                    <NavLink
-                        id={'hw5-pre-junior-link'}
-                        to={PATH.PRE_JUNIOR}
-                        onClick={handleClose}
-                        // className={...} // делает студент
-                    >
-                        Pre-junior
-                    </NavLink>
-                    <NavLink
-                        id={'hw5-junior-link'}
-                        to={PATH.JUNIOR}
-                        onClick={handleClose}
-                        // className={...} // делает студент
-                    >
-                        Junior
-                    </NavLink>
-                    <NavLink
-                        id={'hw5-junior-plus-link'}
-                        to={PATH.JUNIOR_PLUS}
-                        onClick={handleClose}
-                        // className={...} // делает студент
-                    >
-                        Junior Plus
-                    </NavLink>
-                </nav>
-            </aside>
-        </>
-    )
-}
+  return (
+    <>
+      {/*затемнение справа от открытого меню*/}
+      {open && <div className={s.background} onClick={handleClose} />}
+
+      <aside className={sidebarClass}>
+        <button className={s.close} onClick={handleClose}>
+          <img src={closeIcon} alt="close sidebar" id={"hw5-menu-close"} />
+        </button>
+
+        <nav id={"hw5-menu"} className={s.nav}>
+          <NavLink
+            id={"hw5-pre-junior-link"}
+            to={PATH.PRE_JUNIOR}
+            onClick={handleClose}
+            className={!preJuniorClass ? "": s.active}
+
+          >
+            Pre-junior
+          </NavLink>
+          <NavLink
+            id={"hw5-junior-link"}
+            to={PATH.JUNIOR}
+            onClick={handleClose}
+            className={!juniorClass ? "": s.active}
+          >
+            Junior
+          </NavLink>
+          <NavLink
+            id={"hw5-junior-plus-link"}
+            to={PATH.JUNIOR_PLUS}
+            onClick={handleClose}
+            className={!juniorPlusClass ? "": s.active}
+
+          >
+            Junior Plus
+          </NavLink>
+        </nav>
+      </aside>
+    </>
+  );
+};
