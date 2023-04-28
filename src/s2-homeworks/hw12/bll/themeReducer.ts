ddd108674c38dce8aeb12ed89e45f91076dcfae1
dzmitry yermakou <dyermakov@twelvedevs.com>
2023-04-28 10:36:15 +0200

    HW12

--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -1,14 +1,30 @@
 const initState = {
-    themeId: 1,
-}
+  themeId: 1,
+};
+type StateType = {
+  themeId: number;
+};
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
-    switch (action.type) {
-        // дописать
+export const themeReducer = (
+  state: StateType = initState,
+  action: ActionsType
+): StateType => {
+  // fix any
+  switch (action.type) {
+    case "SET_THEME_ID":
+      let newState = { ...state };
+      newState.themeId = action.id;
+      return newState;
+    default:
+      return state;
+  }
+};
 
-        default:
-            return state
-    }
-}
+export const changeThemeId = (id: number): any =>
+  ({
+    type: "SET_THEME_ID",
+    id,
+  } as const); // fix any
+type ChangeThemeIdType = ReturnType<typeof changeThemeId>;
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+type ActionsType = ChangeThemeIdType;
